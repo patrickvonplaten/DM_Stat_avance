8e4ce9e9eaa72ca94514c99f7578ec8232c78e16
pvp23 <patrickvonplaten@MacBook-Pro-de-Patrick.local>
2017-02-20 15:47:23 +0100

    advanced the exercises

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # DM_Stat_avance
 
 This project was done as a two-man student project in the scope of the class advanced statistics.
+
+# TODO Exercises
+(progress of the exercices)
+-[x] Exercise 1
+-[x] Exercise 2
+-[] Exercise 3
+-[] Exercise 4
+-[] Exercise 5
+
+# TODO Rapport
+(progress of the rapport)
+...
+-[] finish Rapport
+-[] proof reading
+
+# ISSUES 
+...
+(if there any things that need to be discussed)
--- src/BROETO-VONPLATEN.R
+++ src/BROETO-VONPLATEN.R
@@ -1,1 +1,16 @@
-new project stat avance...
+# 1)
+#read in table
+df = read.table("Centrale-DM.data", header = TRUE)
+#check observations and variables
+dim(df)
+#rename colname of extra1 to "ABS" and extra2 to "OpenRoof"
+colnames(df)[5] = "ABS"
+colnames(df)[6] = "OpenRoof"
+# 2)
+#make scatterplot
+library(lattice)
+pairs(df[,1:4])
+#make two boxplots
+par(mfrow=c(1,2))
+boxplot(price~ABS,data=df, main = paste("Price ~ f(ABS)"))
+boxplot(price~OpenRoof,data=df, main = paste("Price ~ f(OpenRoof)"))
